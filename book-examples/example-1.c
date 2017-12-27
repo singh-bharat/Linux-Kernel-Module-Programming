3782b7c78d13827eb84de3d06e7267321e36aec4
Bharat Singh <bharatsingh.aitpune@gmail.com>
2017-12-27 15:33:36 +0530

    Delete example-1.c

--- book-examples/example-1.c
+++
@@ -1,9 +0,0 @@
-/* A simple /hello-world/ implementation in kernel module */
-
-#define  MODULE
-#include <linux/module.h>
-
-int init_module (void) { printk ("<1>Hello, World!\n"); return 0; }
-void cleanup_module (void) { printk ("<1>Goodbye, Cruel world!\n");}
-
-
